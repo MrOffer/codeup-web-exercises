ec8fbd571cd576b57900fb3f69fe428aef17b269
MrOffer <thomas.e.offer@gmail.com>
2019-10-18 15:38:18 -0500

    finished math

--- js/circle.js
+++ js/circle.js
@@ -15,7 +15,7 @@
         logInfo: function (doRounding) {
             // TODO: complete this method.
             if (doRounding) {
-                Math.round(circle.getArea());
+                console.log(Math.round(circle.getArea()));
             } else {
                 // If doRounding is true, round the result to the nearest integer.
                 // Otherwise, output the complete value
@@ -26,14 +26,34 @@
     };
 
     // log info about the circle
-    console.log("Raw circle information");
+    console.log("Raw circle information ");
     circle.logInfo(false);
     console.log("Circle information rounded to the nearest whole number");
     circle.logInfo(true);
 
     console.log("=======================================================");
     // TODO: Change the radius of the circle to 5.
+circle = {
+    radius: 5,
+    getArea: function () {
+        // TODO: complete this method
+        // hint: area = pi * radius^2
+        var area = Math.pow(circle.radius, 2) * Math.PI;
+        return area;// TODO: return the proper value
+    },
 
+    logInfo: function (doRounding) {
+        // TODO: complete this method.
+        if (doRounding) {
+            console.log(Math.round(circle.getArea()));
+        } else {
+            // If doRounding is true, round the result to the nearest integer.
+            // Otherwise, output the complete value
+
+            console.log("Area of a circle with radius: " + this.radius + ", is: " + circle.getArea());
+        }
+    }
+};
     // log info about the circle
     console.log("Raw circle information");
     circle.logInfo(false);
