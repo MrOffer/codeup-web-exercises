9a1df75f89351f19d5d0f538dde8e6da7f9837d1
MrOffer <thomas.e.offer@gmail.com>
2019-10-22 09:52:03 -0500

    finished dom query

--- js/morning-warmups.js
+++ js/morning-warmups.js
@@ -1,135 +1,135 @@
-function sum(a, b) {
-    return a + b;
-}
-
-console.log(sum(3, 5));
-
-function tarea(a, b, c) {
-    var parm = (a + b + c) / 2;
-    var trarea = Math.sqrt(parm * (parm - a) * (parm - b) * (parm - c));
-    return trarea;
-}
-
-console.log(tarea(5, 6, 7));
-
-
-function sayhi() {
-    return "Hi";
-}
-
-console.log(sayhi());
-
-function hello(x) {
-    return "Hello " + x + "!";
-}
-
-console.log(hello("David"));
-
-function first(arr) {
-    return arr[0];
-}
-
-console.log(first(["hi", "hello"]))
-
-function ptarea(a, b, c) {
-    var parm = (a + b + c) / 2;
-    var ptrarea = Math.sqrt(parm * (parm - a) * (parm - b) * (parm - c));
-    return ptrarea;
-}
-
-function f(x, y, z) {
-    return x + " " + y + " " + z;
-}
-
-console.log(f(["bat", "elephant"], [1, 2], [5, 6]));
-
-function uppercase(x) {
-    return x.toUpperCase();
-}
-
-console.log(uppercase("a"));
-
-function areaOfCircle(x) {
-    var r = x / 2;
-    return Math.pi * (r * r);
-}
-
-console.log(areaOfCircle(10));
-
-function p(x, y, z, a) {
-    return x + y + z + a;
-}
-
-console.log(p(3, 2, 5, 7));
-
-function f1(x) {
-    return typeof(x);
-}
-
-function positiveEven(x) {
-    return x % 2 === 0 && x > 0;
-}
-console.log(positiveEven(32));
-
-
-function greaterThanFive() {
-    var userNum = parseInt(prompt("Give me a number"));
-    return (userNum > 5);
-}
-console.log(greaterThanFive());
-
-function multiply(num, num1) {
-    return num * num1;
-}
-console.log(multiply());
-
-var multiplyby= function(num, num1) {
-    return num * num1;
-};
-multiplyby();
-
-function helloThere(string = 'Stranger') {
-  return 'Hello there, ' + string + '!';
-}
-var response = prompt('Input string, plox.');
-alert(helloThere(response));
-
-function arrayLength(x) {
- return x.length;
-}
-console.log(arrayLength([3, 5, 6, 8]));
-
-function quotient(x, y) {
-    if (typeof(x) === 'number' && typeof(y) === 'number') {
-        return x / y;
-    } else {
-        return false;
-    }
-}
-console.log(quotient(16, 8));
-
-
-function array1(arr) {
-  return arr;
-}
- console.log(array1([1,2,3,4,5]));
-
-function eight(x) {
-    return x.length >= 8 && typeof x === "string";
-}
- console.log(eight(76868556));
-
-function floatnumber(x) {
-    if (typeof x === 'number'  && !isNaN(x)) {
-        return Math.floor(x);
-    }
-}
-console.log(floatnumber(6.32));
-
-function countUp(x){
-    for (var i = 1; i <= x; i++ ){
-        console.log(i);
-    }
-    return "done counting";
-}
-console.log(countUp(10));
+// function sum(a, b) {
+//     return a + b;
+// }
+//
+// console.log(sum(3, 5));
+//
+// function tarea(a, b, c) {
+//     var parm = (a + b + c) / 2;
+//     var trarea = Math.sqrt(parm * (parm - a) * (parm - b) * (parm - c));
+//     return trarea;
+// }
+//
+// console.log(tarea(5, 6, 7));
+//
+//
+// function sayhi() {
+//     return "Hi";
+// }
+//
+// console.log(sayhi());
+//
+// function hello(x) {
+//     return "Hello " + x + "!";
+// }
+//
+// console.log(hello("David"));
+//
+// function first(arr) {
+//     return arr[0];
+// }
+//
+// console.log(first(["hi", "hello"]))
+//
+// function ptarea(a, b, c) {
+//     var parm = (a + b + c) / 2;
+//     var ptrarea = Math.sqrt(parm * (parm - a) * (parm - b) * (parm - c));
+//     return ptrarea;
+// }
+//
+// function f(x, y, z) {
+//     return x + " " + y + " " + z;
+// }
+//
+// console.log(f(["bat", "elephant"], [1, 2], [5, 6]));
+//
+// function uppercase(x) {
+//     return x.toUpperCase();
+// }
+//
+// console.log(uppercase("a"));
+//
+// function areaOfCircle(x) {
+//     var r = x / 2;
+//     return Math.pi * (r * r);
+// }
+//
+// console.log(areaOfCircle(10));
+//
+// function p(x, y, z, a) {
+//     return x + y + z + a;
+// }
+//
+// console.log(p(3, 2, 5, 7));
+//
+// function f1(x) {
+//     return typeof(x);
+// }
+//
+// function positiveEven(x) {
+//     return x % 2 === 0 && x > 0;
+// }
+// console.log(positiveEven(32));
+//
+//
+// function greaterThanFive() {
+//     var userNum = parseInt(prompt("Give me a number"));
+//     return (userNum > 5);
+// }
+// console.log(greaterThanFive());
+//
+// function multiply(num, num1) {
+//     return num * num1;
+// }
+// console.log(multiply());
+//
+// var multiplyby= function(num, num1) {
+//     return num * num1;
+// };
+// multiplyby();
+//
+// function helloThere(string = 'Stranger') {
+//   return 'Hello there, ' + string + '!';
+// }
+// var response = prompt('Input string, plox.');
+// alert(helloThere(response));
+//
+// function arrayLength(x) {
+//  return x.length;
+// }
+// console.log(arrayLength([3, 5, 6, 8]));
+//
+// function quotient(x, y) {
+//     if (typeof(x) === 'number' && typeof(y) === 'number') {
+//         return x / y;
+//     } else {
+//         return false;
+//     }
+// }
+// console.log(quotient(16, 8));
+//
+//
+// function array1(arr) {
+//   return arr;
+// }
+//  console.log(array1([1,2,3,4,5]));
+//
+// function eight(x) {
+//     return x.length >= 8 && typeof x === "string";
+// }
+//  console.log(eight(76868556));
+//
+// function floatnumber(x) {
+//     if (typeof x === 'number'  && !isNaN(x)) {
+//         return Math.floor(x);
+//     }
+// }
+// console.log(floatnumber(6.32));
+//
+// function countUp(x){
+//     for (var i = 1; i <= x; i++ ){
+//         console.log(i);
+//     }
+//     return "done counting";
+// }
+// console.log(countUp(10));
