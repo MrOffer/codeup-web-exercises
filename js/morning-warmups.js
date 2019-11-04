0d3e79a57d9a5f0a9b263c64fd316561734de250
MrOffer <thomas.e.offer@gmail.com>
2019-11-04 09:25:38 -0600

    finished warmup

--- js/morning-warmups.js
+++ js/morning-warmups.js
@@ -301,3 +301,32 @@
 // //
 // // console.log(hampstersHeight(hamsters));
 
+function numAsWord(num){
+    switch (num) {
+        case 1:
+            return 'one';
+        case 2:
+            return 'two';
+        case 3:
+            return 'three';
+        case 4:
+            return 'four';
+        case 5:
+            return 'five';
+        case 6:
+            return 'six';
+        case 7:
+            return 'seven';
+        case 8:
+            return 'eight';
+        case 9:
+            return 'nine';
+        case 10:
+            return 'ten';
+        default:
+            return false;
+    }
+}
+ var randomNum = Math.floor(Math.random() * 10) +1;
+
+console.log(numAsWord(randomNum));
